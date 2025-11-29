3e90d2313238f63461297d7712282c49ada05f61
prasad-virdande <prasad@ahhaa.com>
2025-11-29 17:44:23 +0530

    added all services

---
+++ order/src/main/java/ecom_order/order/OrderApplication.java
@@ -0,0 +1,13 @@
+package ecom_order.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderApplication.class, args);
+	}
+
+}
